6874403648b365fefe69b32a23b9ad38dda4716e
Juliusz Żwirek <jzwirek96@uczen.tl.krakow.pl>
2026-02-17 09:10:24 +0100

    Created first typescript file

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+const PORT = 3000;
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+})
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+})
